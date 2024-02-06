1a99217ca819850503976d8469460d79c606b03c
chad <chadayop@gmail.com>
2024-02-06 20:53:51 +0800

    Push  century activity

---
+++ centuryFromYear.rb
@@ -0,0 +1,9 @@
+def centuryFromYear(year)
+  return result = (year + 99) / 100
+end
+
+
+puts centuryFromYear(1705)
+puts centuryFromYear(1900)
+puts centuryFromYear(1601)
+puts centuryFromYear(2000)
